34c970b4e39bc9accaa33249ea8f765f43bf52a4
elMike-it <miguel.elizondo@trend-it.com.mx>
2025-01-06 18:43:35 -0600

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "Nombre del bucket creado"
-  value       = google_storage_bucket.test_bucket.name
-}
-
-output "bucket_location" {
-  description = "Región del bucket creado"
-  value       = google_storage_bucket.test_bucket.location
-}
--- terraform/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-project_id   = "test-interno-trendit"
-region       = "us-central1"
-bucket_name  = "mike-terraform"
-force_destroy = true
-retention_days = 1
