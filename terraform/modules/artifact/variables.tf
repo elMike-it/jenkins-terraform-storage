e45a075cf3b6f9d38800ac3fe6c9ddfd686ffa2e
elMike-it <miguel.elizondo@trend-it.com.mx>
2025-01-06 18:45:33 -0600

    Add files via upload

---
+++ terraform/modules/artifact/variables.tf
@@ -0,0 +1,20 @@
+variable "location" {
+  type        = string
+  description = "location ID Vendor Center"
+}
+variable "repository_id" {
+  type        = string
+  description = "repository_id ID Vendor Center"
+}
+variable "description" {
+  type        = string
+  description = "description ID Vendor Center"
+}
+variable "format" {
+  type        = string
+  description = "format ID Vendor Center"
+}
+variable "immutable_tags" {
+  type        = bool
+  description = "immutable_tags ID Vendor Center"
+}
